e98e37b88f6e6b466416ed1a48221a9d1e9cf02b
fmarga <marga.fmartins@gmail.com>
2023-01-04 15:47:03 -0300

    modulo 1, 2 e 3

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
+  .then((response) => response.json())
+  .then((r) => {
+    if (r.error) {
+      throw Error("Esse cep não existe");
+    } else {
+      console.log(r);
+    }
+  })
+  .catch((error) => console.log(error))
+  .finally((message) => console.log("Processamento concluído"));
